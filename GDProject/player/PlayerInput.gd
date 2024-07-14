90af457fab515d8a5c93b5dda688973f1cf0f544
brofessordoucette <brofessordoucette@gmail.com>
2024-07-14 12:11:00 -0500

    Fixed a lot of multiplayer basic movement stuff

--- GDProject/player/PlayerInput.gd
+++ GDProject/player/PlayerInput.gd
@@ -10,11 +10,11 @@ extends MultiplayerSynchronizer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	set_process(get_multiplayer_authority() == multiplayer.get_unique_id())
+	set_physics_process(get_multiplayer_authority() == multiplayer.get_unique_id())
 	set_process_input(get_multiplayer_authority() == multiplayer.get_unique_id())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	
 	if Input.is_action_pressed("Jump"):
 		jumping = true
