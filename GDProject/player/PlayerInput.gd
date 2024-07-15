553291dca78e8f261d306478da607719ba77332d
brofessordoucette <brofessordoucette@gmail.com>
2024-07-14 20:01:17 -0500

    Gave up on client side prediction

--- GDProject/player/PlayerInput.gd
+++ GDProject/player/PlayerInput.gd
@@ -11,7 +11,7 @@ extends MultiplayerSynchronizer
 
 func _ready():
 	
-	set_process(get_multiplayer_authority() == multiplayer.get_unique_id())
+	set_physics_process(get_multiplayer_authority() == multiplayer.get_unique_id())
 	set_process_input(get_multiplayer_authority() == multiplayer.get_unique_id())
 
 func _input(event):
